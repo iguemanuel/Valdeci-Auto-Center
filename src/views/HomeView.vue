7435c8a0cc037c04a04a3bb270f10595434efac5
Igor <igor17emanuel@gmail.com>
2026-02-02 11:13:22 -0300

    Add Navbar components with responsive design

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import CarouselComponent from '@/components/CarouselComponent.vue'
+
+import banner01 from '@/assets/banner01.jpeg'
+import banner02 from '@/assets/banner02.png'
+const slides = [
+  {
+    id: 1,
+    image: banner01,
+    alt: 'Serviços automotivos de qualidade',
+  },
+  {
+    id: 2,
+    image: banner02,
+    alt: 'Curso de mecânica automotiva',
+  },
+]
+</script>
+
+<template>
+  <div class="min-h-screen bg-base-100">
+    <CarouselComponent :slides="slides" />
+  </div>
+</template>
+
+<style scoped></style>
